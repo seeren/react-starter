f1b3c9bc416e700e1262e2028a148a31652ea2af
seeren <consultant@seeren.fr>
2022-12-16 23:06:48 +0100

    Setup eslint and pretier

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,21 @@
-import { ReportHandler } from 'web-vitals';
+// eslint-disable-next-line import/named
+import { ReportHandler } from 'web-vitals'
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry)
+        getFID(onPerfEntry)
+        getFCP(onPerfEntry)
+        getLCP(onPerfEntry)
+        getTTFB(onPerfEntry)
+        return
+      })
+      .catch((error: Error) => {
+        throw error
+      })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
