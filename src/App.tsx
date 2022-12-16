9594cf25ea440d4faed56332b30e0e659eb57da8
seeren <consultant@seeren.fr>
2022-12-17 00:13:14 +0100

    Register first rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           ffff <code>src/Apzsp.tsx</code> cc dd to barrrr
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
