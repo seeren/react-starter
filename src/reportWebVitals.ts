1114ea2785d1823a3df2c1deb6b567aac7b7b79a
seeren <consultant@seeren.fr>
2022-12-17 20:20:32 +0100

    Fix lint

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,21 +1,21 @@
 // eslint-disable-next-line import/named
-import { ReportHandler } from 'web-vitals'
+import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals')
       .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-        getCLS(onPerfEntry)
-        getFID(onPerfEntry)
-        getFCP(onPerfEntry)
-        getLCP(onPerfEntry)
-        getTTFB(onPerfEntry)
-        return
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+        return;
       })
       .catch((error: Error) => {
-        throw error
-      })
+        throw error;
+      });
   }
-}
+};
 
-export default reportWebVitals
+export default reportWebVitals;
