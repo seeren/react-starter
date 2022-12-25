070e8f5dfd56a70094568e3eb6d897c9d55e5dc1
seeren <consultant@seeren.fr>
2022-12-25 15:05:31 +0100

    Embeed style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import 'src/assets/scss/index.scss';
 
 import React, { ReactElement } from 'react';
 
 import { Routes } from 'src/routes';
+import 'src/assets/scss/index.scss';
 
 function App(): ReactElement {
   return <Routes />;
